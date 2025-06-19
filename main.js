8c519254cc727e8cfe1bfce84fd77576ca9bf84b
Iniyavan A <89763278+Iniyavan-Appu@users.noreply.github.com>
2025-06-19 20:21:44 +0530

    Add files via upload
    New project created for the DOM

---
+++ main.js
@@ -0,0 +1,48 @@
+// localStorage.setItem('Username', 'iniyavan');
+// localStorage.setItem('Desigination', 'Developer');
+
+// addEventListener("click", )
+
+let button = document.querySelector(".button");
+
+// button.addEventListener("click", () => console.log("Handler 1"));
+// button.addEventListener("click", () => console.log("Handler 2"));
+
+// button.addEventListener("click", function () {
+//     customFunction("Hello");
+// });
+
+// function customFunction(msg) {
+//     console.log(msg);
+// }
+// customFunction("Hello");
+
+// let a = 8;
+// function add(b) {
+//   return a + b;
+// }
+// add(5)
+
+// let fruits = ["apple", "banana"];
+let c = document.querySelector(".ini");
+
+let result = true;
+
+if (result) {
+    c.textContent = `pass`;
+}
+else {
+
+    c.textContent = `fail`;
+}
+
+// c.innerHTML = `<h1>hello</h1>`;
+// c.textContent = `fail`;
+
+// c.innerHTML = fruits.map(a => `<li>${a}</li>`).join('');
+
+// let a = document.getElementsByClassName("ini");
+// let b = document.querySelector(".ini");
+// console.log(a);
+// console.log(b);
+console.log(c);
